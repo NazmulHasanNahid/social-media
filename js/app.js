b024060c0faa9fa40e9f93d956c992fb284e399e
NazmulHasanNahid <nazmulhasan3394@gmail.com>
2022-03-10 22:59:55 +0600

    text errror

--- js/app.js
+++ js/app.js
@@ -27,7 +27,7 @@ const reportPost = (id) => {
 };
 
 const displayContent = (text) => {
-    return text.length < 30 ? 'text' : text.slice(0, 30)  + "<span class='fw-bold'>... read more</span>";
+    return text.length < 30 ? text : text.slice(0, 30)  + "<span class='fw-bold'>... read more</span>";  
     
 };
 
@@ -96,9 +96,7 @@ const createPost = (post) => {
 
                   <div class="post__indicators"></div>
 
-                  <button class="post__button post__button--align-right" onclick="reportPost(${
-                      post.id
-                  })">
+                  <button class="post__button post__button--align-right" onclick="reportPost(${post.id})">
                     <i class="fa-solid fa-ban"></i>
                   </button>
                 </div>
