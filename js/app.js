69d6c4163467000e4528921c0c82eb902ce0bbea
NazmulHasanNahid <nazmulhasan3394@gmail.com>
2022-03-11 20:32:24 +0600

    style

--- js/app.js
+++ js/app.js
@@ -41,12 +41,15 @@ const switchTab = (id) => {
         document.getElementById( "posts" ).style.display = "grid";
         document.getElementById( "liked" ).style.display = "none";
         document.getElementById( "reported" ).style.display = "none";
+        document.getElementById( "question-section" ).style.display = "block";
        
       
     } else if (id === "liked") {
         document.getElementById( "liked" ).style.display = "block";
         document.getElementById( "posts" ).style.display = "none";
         document.getElementById( "reported" ).style.display = "none";
+        document.getElementById( "question-section" ).style.display = "none";
+
        
 
         displayLikedPosts();
@@ -54,6 +57,8 @@ const switchTab = (id) => {
         document.getElementById( "reported" ).style.display = "block";
         document.getElementById( "posts" ).style.display = "none";
         document.getElementById( "liked" ).style.display = "none";
+        document.getElementById( "question-section" ).style.display = "none";
+
         
 
         displayReportedPosts();
